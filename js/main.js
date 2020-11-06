29bbbe493dd00aca5b81e7d891a9636f4e69012a
Lucas <lucas98fernando@gmail.com>
2020-11-05 23:13:26 -0300

    More layout adjustements

--- js/main.js
+++ js/main.js
@@ -20,25 +20,34 @@ if (i == 0) {
     document.getElementById("prev").style.display = "none";
 }
 
+if (i == 4) {
+    document.getElementById("next").style.display = "none";
+}
+
 // Function to add click event
 next.onclick = function () {
     // Limiting slide amount
     if (i < 4) {
-        hero.style.backgroundImage = 'url(" '+backgroundImg[i + 1]+' ")';
+        hero.style.backgroundImage = 'url(" ' + backgroundImg[i + 1] + ' ")';
         thumbnail[i + 1].classList.add("active");
         thumbnail[i].classList.remove("active");
         i++;
     }
-    // Show left arrow before first thumbnail
+    // Show left arrow after first thumbnail
     if (i > 0) {
         document.getElementById("prev").style.display = "block";
+    }
+
+    // Hide right arrow on last thumbnail
+    if (i > 3) {
+        document.getElementById("next").style.display = "none";
     }
 }
 
 prev.onclick = function () {
     // Limiting slide amount
     if (i > 0) {
-        hero.style.backgroundImage = 'url(" '+backgroundImg[i - 1]+' ")';
+        hero.style.backgroundImage = 'url(" ' + backgroundImg[i - 1] + ' ")';
         thumbnail[i - 1].classList.add("active");
         thumbnail[i].classList.remove("active");
         i--;
@@ -46,5 +55,10 @@ prev.onclick = function () {
     // Hide left arrow on first thumbnail
     if (i == 0) {
         document.getElementById("prev").style.display = "none";
+    }
+
+    // Show right arrow again from the third thumbnail 
+    if (i < 4) {
+        document.getElementById("next").style.display = "block";
     }
 }
